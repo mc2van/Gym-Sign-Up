4b3dafb3d10bae91fc96bb7cf35bfb6732cbf91d
Evan Qi <evanqi@gmail.com>
2021-01-12 15:10:25 -0500

    Update part2.py

--- part2.py
+++ part2.py
@@ -42,5 +42,24 @@ driver2.implicitly_wait(10) #10 is in seconds
 # to load a given URL in browser window
 driver2.get(base_url)
 
+#click on the sign up initial
+# FILL IN HERE
+
+# "no" to covid buttons
+
+buttons = driver.find_element_by_css_selector('input#rbtnNo')
+
+for button in buttons:
+    button.click
+
+# submit
+
+driver.find_element_by_css_selector('button.btn.btn-primary').click()
+
+# submit covid waiver
+
+driver.find_element_by_id('btnAccept').click()
+
+
 # https://warrior.uwaterloo.ca/CustomPromptsAddon/GetCustomPrompts?relatedRegistrationId=f1edc478-6a13-42d0-ac50-26dfcf5273d0&productType=00000000-0000-0000-0000-000000003502
 # https://warrior.uwaterloo.ca/Waiver?productId=cc2a16d7-f148-461e-831d-7d4659726dd1&entityTypeId=00000000-0000-0000-0000-000000003586&partyId=a9a83df3-431f-49a9-96c5-f308fb1b26b1&relatedEntityId=00000000-0000-0000-0000-000000000000
