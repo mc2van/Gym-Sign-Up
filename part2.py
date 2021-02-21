ba6ce2ca70b1da9c382372eb8066dcaf3e21d6e3
Evan Qi <evanqi@gmail.com>
2021-02-21 11:31:35 -0500

    Update part2

--- part2.py
+++ part2.py
@@ -4,29 +4,31 @@ from selenium.webdriver.common.keys import Keys
 base_url="https://warrior.uwaterloo.ca/Program/GetProgramDetails?courseId=cc2a16d7-f148-461e-831d-7d4659726dd1&semesterId=b0d461c3-71ea-458e-b150-134678037221"
 driver = webdriver.Chrome()
 
-def create_driver_session(session_id, executor_url):
-    from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 
-    # Save the original function, so we can revert our patch
-    org_command_execute = RemoteWebDriver.execute
+# def create_driver_session(session_id, executor_url):
+#     from selenium.webdriver.remote.webdriver import WebDriver as RemoteWebDriver
 
-    def new_command_execute(self, command, params=None):
-        if command == "newSession":
-            # Mock the response
-            return {'success': 0, 'value': None, 'sessionId': session_id}
-        else:
-            return org_command_execute(self, command, params)
+#     # Save the original function, so we can revert our patch
+#     org_command_execute = RemoteWebDriver.execute
 
-    # Patch the function before creating the driver object
-    RemoteWebDriver.execute = new_command_execute
+#     def new_command_execute(self, command, params=None):
+#         if command == "newSession":
+#             # Mock the response
+#             return {'success': 0, 'value': None, 'sessionId': session_id}
+#         else:
+#             return org_command_execute(self, command, params)
 
-    new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
-    new_driver.session_id = session_id
+#     # Patch the function before creating the driver object
+#     RemoteWebDriver.execute = new_command_execute
 
-    # Replace the patched function with original function
-    RemoteWebDriver.execute = org_command_execute
+#     new_driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
+#     new_driver.session_id = session_id
 
-    return new_driver
+#     # Replace the patched function with original function
+#     RemoteWebDriver.execute = org_command_execute
+
+#     return new_driver
+
 
 driver.close()
 
@@ -35,15 +37,27 @@ file = open('saved.txt', 'r')
 filelines = file.readlines()
 session_id = filelines[0].strip()
 executor_url = filelines[1]
+print(session_id)
+print(executor_url)
 
-driver2 = create_driver_session(session_id, executor_url)
+driver = webdriver.Remote(command_executor=executor_url,desired_capabilities={})
+driver.close()
+driver.session_id = session_id
+
+
+# driver2 = create_driver_session(session_id, executor_url) - outdated
 # browser should be loaded in maximized window
-driver2.implicitly_wait(10) #10 is in seconds
+driver.implicitly_wait(10) #10 is in seconds
 # to load a given URL in browser window
-driver2.get(base_url)
+driver.get(base_url)
 
 #click on the sign up initial
-# FILL IN HERE
+
+driver.find_element_by_css_selector(".btn.btn-primary").click()
+
+# initial accept thing
+
+driver.find_element_by_id('btnAccept').click()
 
 # "no" to covid buttons
 
@@ -52,13 +66,13 @@ buttons = driver.find_element_by_css_selector('input#rbtnNo')
 for button in buttons:
     button.click
 
-# submit
+# submit the covid waiver (click no's)
 
 driver.find_element_by_css_selector('button.btn.btn-primary').click()
 
 # submit covid waiver
 
-driver.find_element_by_id('btnAccept').click()
+# driver.find_element_by_id('checkoutButton').click()
 
 
 # https://warrior.uwaterloo.ca/CustomPromptsAddon/GetCustomPrompts?relatedRegistrationId=f1edc478-6a13-42d0-ac50-26dfcf5273d0&productType=00000000-0000-0000-0000-000000003502
